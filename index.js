e52deac825b93023334d4b0ee93cb993485ee350
Duane Fields <duane@duanefields.com>
2014-09-03 07:58:34 -0500

    added navbar and togglers

--- index.js
+++ index.js
@@ -3,16 +3,54 @@ angular.module('cardsApp', [])
     $scope.cards = new Array();
     $scope.showAllCards = true;
 
+    // terrain cards
     for (i=1; i <= 67; i++) {
       card = {
         //src: "http://static.memoir44.com/lang/english/images/mm_compendium_terrain_" + i +".jpg",
+        number: i,
         src: "http://static.daysofwonder.com/memoir44/en/img/mm_compendium_terrain_" + i + ".jpg",
-        selected: i % 2 == 0
+        selected: true
       };
       $scope.cards.push(card)
     }
 
-    $scope.toggle = function() {
+    // troop cards
+    for (i=1; i <= 26; i++) {
+      card = {
+        number: i,
+        src: "http://static.daysofwonder.com/memoir44/en/img/mm_compendium_troop_" + i + ".jpg",
+        selected: true
+      };
+      $scope.cards.push(card)
+    }
+
+    // swa cards
+    for (i=1; i <= 8; i++) {
+      card = {
+        number: i,
+        src: "http://static.daysofwonder.com/memoir44/en/img/mm_compendium_swa_" + i + ".jpg",
+        selected: true
+      };
+      $scope.cards.push(card)
+    }
+
+    $scope.toggle = function(card) {
+      card.selected = ! card.selected;
     };
+
+    $scope.setAll = function(selected) {
+      for (i=0; i < $scope.cards.length; i++) {
+        $scope.cards[i].selected = selected;
+      }
+    };
+
+    $scope.selectAll = function() {
+      $scope.setAll(true);
+    };
+
+    $scope.selectNone = function() {
+      $scope.setAll(false);
+    };
+
 
   }]);
